fbe0623eb9e766f594d4aa732385790a2a663b60
jdkelkel <blin.kelvinsky@gmail.com>
2026-08-15 22:00:07 +0300

    Update main.go
    +рандомайзер
    + обновления
    +команда

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 
 	bot, err := tgbotapi.NewBotAPI("")
 	if err != nil {
 		log.Panic(err)
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.Text == "/startgame" {
+			StartGame(bot, update)
+		}
 	}
 }
