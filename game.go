0edbb1ffb624ba79a6f36ad5ad87a0bc6a3d2040
jdkelkel <blin.kelvinsky@gmail.com>
2026-08-16 22:20:13 +0300

    Update game.go

--- game.go
+++ game.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+    "strings"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+func isCorrectRussian(input string, w Word) bool {
+    return strings.ToLower(strings.TrimSpace(input)) ==
+        strings.ToLower(strings.TrimSpace(w.Russian))
+}
+
 func StartGame(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-
     chatID := update.Message.Chat.ID
-
     current := RandomWord()
 
     bot.Send(tgbotapi.NewMessage(chatID,
         "Переведи слово:\nLatin: "+current.Latin+"\nCyril: "+current.Cyril))
+
+    u := tgbotapi.NewUpdate(update.UpdateID + 1)
+    u.Timeout = 60
+    updates := bot.GetUpdatesChan(u)
+
+
+    for upd := range updates {
+        if upd.Message == nil {
+            continue
+        }
+
+        userText := upd.Message.Text
+
+        if isCorrectRussian(userText, current) {
+
+            bot.Send(tgbotapi.NewMessage(chatID, "Верно!"))
+        } else {
+
+            bot.Send(tgbotapi.NewMessage(chatID, "Неверно. Попробуй ещё раз."))
+        }
+    }
 }
