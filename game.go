ac0fb4365c1ef3eca781ceab552192d515d5762a
jdkelkel <blin.kelvinsky@gmail.com>
2026-08-18 18:39:16 +0300

    Update game.go
    исправил ошибку

--- game.go
+++ game.go
@@ -1,43 +1,32 @@
 package main
 
 import (
-	"strings"
+    "strings"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func isCorrectRussian(input string, w Word) bool {
-	return strings.ToLower(strings.TrimSpace(input)) ==
-		strings.ToLower(strings.TrimSpace(w.Russian))
+    return strings.ToLower(strings.TrimSpace(input)) ==
+        strings.ToLower(strings.TrimSpace(w.Russian))
 }
 
-func StartGame(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	chatID := update.Message.Chat.ID
+func StartGame(bot *tgbotapi.BotAPI, chatID, userID int64) {
+    current := RandomWord()
+    userState[userID] = current
 
-	current := RandomWord()
+    msg := "Переведи слово:\nLatin: " + current.Latin + "\nCyril: " + current.Cyril
+    bot.Send(tgbotapi.NewMessage(chatID, msg))
+}
 
-	bot.Send(tgbotapi.NewMessage(chatID,
-		"Переведи слово:\nLatin: "+current.Latin+"\nCyril: "+current.Cyril))
+func HandleGameMessage(bot *tgbotapi.BotAPI, chatID, userID int64, text string, current Word) {
+    if isCorrectRussian(text, current) {
+        newWord := RandomWord()
+        userState[userID] = newWord
 
-	u := tgbotapi.NewUpdate(update.UpdateID + 1)
-	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
-
-	for upd := range updates {
-		if upd.Message == nil {
-			continue
-		}
-
-		userText := upd.Message.Text
-
-		if isCorrectRussian(userText, current) {
-			current = RandomWord()
-
-			bot.Send(tgbotapi.NewMessage(chatID,
-				"Верно! Новое слово:\nLatin: "+current.Latin+"\nCyril: "+current.Cyril))
-		} else {
-			bot.Send(tgbotapi.NewMessage(chatID,
-				"Неверно. Попробуй ещё раз."))
-		}
-	}
+        msg := "Верно! Новое слово:\nLatin: " + newWord.Latin + "\nCyril: " + newWord.Cyril
+        bot.Send(tgbotapi.NewMessage(chatID, msg))
+    } else {
+        bot.Send(tgbotapi.NewMessage(chatID, "Неверно. Попробуй ещё раз."))
+    }
 }
